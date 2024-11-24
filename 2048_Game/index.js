c95d4d17bf730e95f0bfa195254bd07e10418b3f
rajsainideveloper <rajsainideveloper@gmail.com>
2024-11-25 00:32:20 +0530

    update

--- 2048_Game/index.js
+++ 2048_Game/index.js
@@ -16,43 +16,16 @@ let gameArray = [
 // gameArray[0][0] = 2;
 // gameArray[0][1] = 2;
 let check_game_is_over = true;
+// calculateData();
 
-calculateData();
-
-function calculateData() {
-    check_game_is_over = true;
-    for( let i=0; i<gameArray.length; i++) {
-        for( let j=0; j<=3; j++) {
-            if ( gameArray[i][j] != [] && gameArray[i][j] == gameArray[i][j+1]) {
-                let sum = gameArray[i][j]+gameArray[i][j+1];
-                gameArray[i][j+1] = sum;
-                gameArray[i][j] = [];
-            }
-            //check game is over or not
-            if ( gameArray[i][j] == [] || gameArray[i][j] == '') {
-                check_game_is_over = false;
-            }
-        } 
-        // console.log("------------");
-    }
-}
-
-
-
-
+genrateTwoRandomPosition();
 renderTheData();
-function renderTheData() {
-    for( let i=0; i<gameArray.length; i++) {
-        for( let j=0; j<=3; j++) {
-            let class_name = i+"_"+j;
-            document.getElementsByClassName(class_name)[0].innerHTML = gameArray[i][j];
-        } 
-    }
-}
+
 
 
 document.addEventListener('keydown', function(e) {
     e.preventDefault(); // Prevent the default scrolling behavior
+    calculateData();
     if ( check_game_is_over ) {
         showWarning("Game is Over Now...");
         // document.getElementsByClassName('over_warnging')[0].innerHTML = "Game is Over Now......";
@@ -81,6 +54,15 @@ document.addEventListener('keydown', function(e) {
     }
 });
 
+function renderTheData() {
+    for( let i=0; i<gameArray.length; i++) {
+        for( let j=0; j<=3; j++) {
+            let class_name = i+"_"+j;
+            document.getElementsByClassName(class_name)[0].innerHTML = gameArray[i][j];
+        } 
+    }
+}
+
 
 function putNumber( number ) {
     
@@ -88,7 +70,7 @@ function putNumber( number ) {
         for( let j=0; j<=3; j++) {
             if( gameArray[i][j] == [] || gameArray[i][j] == '') {
                 // gameArray[i][j]
-                console.log();
+                // console.log();
                 let class_name = i+"_"+j;
                 // document.getElementsByClassName(class_name)[0].innerHTML = number;
                 gameArray[i][j] = number;
@@ -105,4 +87,37 @@ function putNumber( number ) {
 
 function showWarning(text) {
     document.getElementsByClassName('over_warnging')[0].innerHTML = text;
+}
+
+function genrateTwoRandomPosition() {
+    let randomNumber1,randomNumber2 = 0;
+    randomNumber1 = Math.floor(Math.random() * 4);
+    randomNumber2 = Math.floor(Math.random() * 4);
+    gameArray[randomNumber1][randomNumber2] = 2;
+
+    randomNumber1 = Math.floor(Math.random() * 4);
+    randomNumber2 = Math.floor(Math.random() * 4);
+    if ( gameArray[randomNumber1][randomNumber2] != '') {
+        gameArray[randomNumber2][randomNumber1] = 2;
+    } else {
+        gameArray[randomNumber1][randomNumber2] = 2;
+    }
+}
+
+function calculateData() {
+    check_game_is_over = true;
+    for( let i=0; i<gameArray.length; i++) {
+        for( let j=0; j<=3; j++) {
+            if ( gameArray[i][j] != [] && gameArray[i][j] == gameArray[i][j+1]) {
+                let sum = gameArray[i][j]+gameArray[i][j+1];
+                gameArray[i][j+1] = sum;
+                gameArray[i][j] = [];
+            }
+            //check game is over or not
+            if ( gameArray[i][j] == [] || gameArray[i][j] == '') {
+                check_game_is_over = false;
+            }
+        } 
+        // console.log("------------");
+    }
 }
